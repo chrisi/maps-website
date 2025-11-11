607cc5b2828d0f72344df084213c33ad6ff027db
Christian Gebauer <mail@christiangebauer.de>
2025-11-11 14:24:43 +0100

    more data migration

--- src/data/stations.ts
+++ src/data/stations.ts
@@ -2,7 +2,12 @@ import type {Station} from "@/model/Station.ts";
 
 export const stations: Station[] = [
   {
-    posx: 958, posy: 1332, size: 8, country: 'South Korea', name: 'CHEONGJU', type: 'Airbase',
+    posx: 958,
+    posy: 1332,
+    size: 8,
+    country: 'South Korea',
+    name: 'CHEONGJU',
+    type: 'Airbase',
     details: {
       name: "CHEONGJU (RKTU)",
       lat: "N36° 42.857'",
@@ -60,7 +65,12 @@ export const stations: Station[] = [
     }
   },
   {
-    posx: 1158, posy: 1502, size: 8, country: 'South Korea', name: 'DAEGU', type: 'Airbase',
+    posx: 1158,
+    posy: 1502,
+    size: 8,
+    country: 'South Korea',
+    name: 'DAEGU',
+    type: 'Airbase',
     details: {
       name: "DAEGU (RKTN)",
       lat: "N35°53.672'",
@@ -359,17 +369,584 @@ export const stations: Station[] = [
       ]
     }
   },
-  {posx: 841, posy: 1662, size: 8, country: 'South Korea', name: 'GWANGJU', type: 'Airbase'},
-  {posx: 787, posy: 1173, size: 8, country: 'South Korea', name: 'INCHEON', type: 'Airbase'},
-  {posx: 1023, posy: 1267, size: 8, country: 'South Korea', name: 'JUNGWON', type: 'Airbase'},
-  {posx: 1188, posy: 1253, size: 8, country: 'South Korea', name: 'KOTAR', type: 'Airbase'},
-  {posx: 769, posy: 1688, size: 8, country: 'South Korea', name: 'MUAN', type: 'Airbase'},
-  {posx: 880, posy: 1252, size: 6, country: 'South Korea', name: 'OSAN', type: 'Airbase'},
-  {posx: 1283, posy: 1480, size: 6, country: 'South Korea', name: 'POHANG', type: 'Airbase'},
-  {posx: 882, posy: 1280, size: 8, country: 'South Korea', name: 'PYEONGTAEK', type: 'Airbase'},
-  {posx: 1057, posy: 1671, size: 8, country: 'South Korea', name: 'SACHEON', type: 'Airbase'},
-  {posx: 790, posy: 1332, size: 8, country: 'South Korea', name: 'SEOSAN', type: 'Airbase'},
-  {posx: 896, posy: 1180, size: 8, country: 'South Korea', name: 'SEOUL', type: 'Airbase'},
+  {
+    posx: 841,
+    posy: 1662,
+    size: 8,
+    country: 'South Korea',
+    name: 'GWANGJU',
+    type: 'Airbase',
+    details: {
+      name: "GWANGJU (RKJJ)",
+      lat: "N35°07.916'",
+      long: "E126°48.349'",
+      elev: "42 ft",
+      rwy: "04L/22R - 04R/22L",
+      tcn: "91X",
+      atis: "128.875",
+      ops: "", // Not provided in the HTML
+      gnd: "275.8",
+      twr: "254.6",
+      appdep: "268.0",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - ADC.pdf",
+          page: 1
+        },
+        {
+          name: "Visual Approach",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - VISUAL.pdf",
+          page: 1
+        },
+        {
+          name: "EOR Procedure Chart",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ)_EOR.png",
+          page: 1
+        },
+        {
+          name: "ILS RWY 04R (111.1)",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - ILS.pdf",
+          page: 1
+        },
+        {
+          name: "RNP RWY 04L",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - ILS.pdf",
+          page: 13
+        },
+        {
+          name: "RNP RWY 22L",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - ILS.pdf",
+          page: 23
+        },
+        {
+          name: "TACAN RWY 04L",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - ILS.pdf",
+          page: 15
+        },
+        {
+          name: "TACAN RWY 22L",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - ILS.pdf",
+          page: 21
+        },
+        {
+          name: "Standard Arrivals",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - STAR.pdf",
+          page: 1
+        },
+        {
+          name: "Standard Departures",
+          url: "01 South Korea (ROK-US)/Gwangju AB (RKJJ)/Gwangju AB (RKJJ) - SID.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 787,
+    posy: 1173,
+    size: 8,
+    country: 'South Korea',
+    name: 'INCHEON',
+    type: 'Airbase',
+    details: {
+      name: "INCHEON (RKSI)",
+      lat: "N37°28.430'",
+      long: "E126°27.083'",
+      elev: "18 ft",
+      rwy: "15L/33R-15R/33L",
+      tcn: "76X",
+      atis: "128.45",
+      ops: "", // Not provided in the HTML
+      gnd: "266.925",
+      twr: "231.8",
+      appdep: "293.225",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - ADC.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 15L (111.9)",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - ILS.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 15R (109.1)",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - ILS.pdf",
+          page: 9
+        },
+        {
+          name: "ILS RWY 33L (109.3)",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - ILS.pdf",
+          page: 23
+        },
+        {
+          name: "ILS RWY 33R (108.9)",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - ILS.pdf",
+          page: 27
+        },
+        {
+          name: "Visual Approach",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - VISUAL.pdf",
+          page: 1
+        },
+        {
+          name: "Standard Arrivals",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - STAR.pdf",
+          page: 1
+        },
+        {
+          name: "Standard Departures",
+          url: "01 South Korea (ROK-US)/Incheon Intl (RKSI)/Incheon Intl (RKSI) - SID.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 1023,
+    posy: 1267,
+    size: 8,
+    country: 'South Korea',
+    name: 'JUNGWON',
+    type: 'Airbase',
+    details: {
+      name: "JUNGWON (RKTI)",
+      lat: "N37°01.699'",
+      long: "E127°53.116'",
+      elev: "290 ft",
+      rwy: "18L/36R - 18R/36L",
+      tcn: "05X",
+      atis: "135.6",
+      ops: "",
+      gnd: "275.9",
+      twr: "230.15",
+      appdep: "306.7",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea/Jungwon/Jungwon_Airport.png",
+          page: 1
+        },
+        {
+          name: "EOR Procedure Chart",
+          url: "01 South Korea (ROK-US)/Jungwon AB (RKTI)/Jungwon AB (RKTI)_EOR.png",
+          page: 1
+        },
+        {
+          name: "ILS RWY 36R (111.3)",
+          url: "01 South Korea (ROK-US)/Jungwon AB (RKTI)/Jungwon AB (RKTI) - ilsdme_rwy_36r.pdf",
+          page: 1
+        },
+        {
+          name: "TACAN RWY 36L",
+          url: "01 South Korea (ROK-US)/Jungwon AB (RKTI)/Jungwon AB (RKTI) - tacan_rwy_36l.pdf",
+          page: 1
+        },
+        {
+          name: "TACAN RWY 36R",
+          url: "01 South Korea (ROK-US)/Jungwon AB (RKTI)/Jungwon AB (RKTI) - tacan_rwy_36r.pdf",
+          page: 1
+        },
+        {
+          name: "Jungwon 2 Departure",
+          url: "01 South Korea (ROK-US)/Jungwon AB (RKTI)/Jungwon AB (RKTI) - jungwon_dep.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 1188,
+    posy: 1253,
+    size: 8,
+    country: 'South Korea',
+    name: 'KOTAR',
+    type: 'Airbase'
+  },
+  {
+    posx: 769,
+    posy: 1688,
+    size: 8,
+    country: 'South Korea',
+    name: 'MUAN',
+    type: 'Airbase',
+    details: {
+      name: "MUAN (RKJB)",
+      lat: "N35°00.059'",
+      long: "E126°23.191'",
+      elev: "44 ft",
+      rwy: "01/19",
+      tcn: "47X",
+      atis: "127.425",
+      ops: "",
+      gnd: "231.7",
+      twr: "228.25",
+      appdep: "240.0",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea (ROK-US)/Muan Intl (RKJB)/Muan Intl (RKJB) - ADC.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 01 (111.9)",
+          url: "01 South Korea (ROK-US)/Muan Intl (RKJB)/Muan Intl (RKJB) - ILS.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 19 (108.9)",
+          url: "01 South Korea (ROK-US)/Muan Intl (RKJB)/Muan Intl (RKJB) - ILS.pdf",
+          page: 11
+        },
+        {
+          name: "Visual Approach",
+          url: "01 South Korea (ROK-US)/Muan Intl (RKJB)/Muan Intl (RKJB) - VISUAL.pdf",
+          page: 1
+        },
+        {
+          name: "Standard Arrivals",
+          url: "01 South Korea (ROK-US)/Muan Intl (RKJB)/Muan Intl (RKJB) - STAR.pdf",
+          page: 1
+        },
+        {
+          name: "Standard Departures",
+          url: "01 South Korea (ROK-US)/Muan Intl (RKJB)/Muan Intl (RKJB) - SID.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 880,
+    posy: 1252,
+    size: 6,
+    country: 'South Korea',
+    name: 'OSAN',
+    type: 'Airbase',
+    details: {
+      name: "OSAN AB (RKSO)",
+      lat: "N37°04.862'",
+      long: "E127°01.386'",
+      elev: "97 ft",
+      rwy: "09L/27R - 09R/27L",
+      tcn: "94X",
+      atis: "132.125",
+      ops: "",
+      gnd: "253.7",
+      twr: "308.8",
+      appdep: "306.3",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "https://cdn.falcon-bms.com/maps/01_KTO/charts/01 South Korea/Osan/Osan_Airport.png",
+          page: 1
+        },
+        {
+          name: "EOR Procedure Chart",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO)_EOR.png",
+          page: 1
+        },
+        {
+          name: "ILS RWY 09L (111.3)",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO) - ils_or_locdme_rwy_09l.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 27R (111.3)",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO) - ils_or_locdme_rwy_27r.pdf",
+          page: 1
+        },
+        {
+          name: "RNAV RWY 09L",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO) - rnav_gps_rwy_09l.pdf",
+          page: 1
+        },
+        {
+          name: "RNAV RWY 27R",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO) - rnav_gps_rwy_27r.pdf",
+          page: 1
+        },
+        {
+          name: "DRAGGIN 8 Departure",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO) - draggin_dep.pdf",
+          page: 1
+        },
+        {
+          name: "MUSTANG 4 Departure",
+          url: "01 South Korea (ROK-US)/Osan AB (RKSO)/Osan AB (RKSO) - mustang_dep.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 1283,
+    posy: 1480,
+    size: 6,
+    country: 'South Korea',
+    name: 'POHANG',
+    type: 'Airbase',
+    details: {
+      name: "POHANG (RKTH)",
+      lat: "N35°59.123'",
+      long: "E129°25.200'",
+      elev: "74 ft",
+      rwy: "10/28",
+      tcn: "72X",
+      atis: "127.4",
+      ops: "317.375",
+      gnd: "275.4",
+      twr: "236.6",
+      appdep: "232.4",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea (ROK-US)/Pohang AB (RKTH)/Pohang AB (RKTH) - ADC.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 10 (110.9)",
+          url: "01 South Korea (ROK-US)/Pohang AB (RKTH)/Pohang AB (RKTH) - ILS.pdf",
+          page: 1
+        },
+        {
+          name: "TACAN RWY 10",
+          url: "01 South Korea (ROK-US)/Pohang AB (RKTH)/Pohang AB (RKTH) - ILS.pdf",
+          page: 3
+        },
+        {
+          name: "TACAN RWY 28",
+          url: "01 South Korea (ROK-US)/Pohang AB (RKTH)/Pohang AB (RKTH) - ILS.pdf",
+          page: 9
+        },
+        {
+          name: "Standard Departures",
+          url: "01 South Korea (ROK-US)/Pohang AB (RKTH)/Pohang AB (RKTH) - SID.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 882,
+    posy: 1280,
+    size: 8,
+    country: 'South Korea',
+    name: 'PYEONGTAEK',
+    type: 'Airbase',
+    details: {
+      name: "PYEONGTAEK AF (RKSG)",
+      lat: "N36°57.801'",
+      long: "E127°01.873'",
+      elev: "48 ft",
+      rwy: "14/32",
+      tcn: "19X",
+      atis: "128.25",
+      ops: "291.1",
+      gnd: "229.7",
+      twr: "257.8",
+      appdep: "363.1",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea/Pyeongtaek/Pyeongtaek_Airport.png",
+          page: 1
+        },
+        {
+          name: "ILS RWY 32 (108.75)",
+          url: "01 South Korea (ROK-US)/Pyeongtaek AAF (RKSG)/Pyeongtaek AAF (RKSG) - ils_or_loc_rwy_32.pdf",
+          page: 1
+        },
+        {
+          name: "DVOR RWY 32",
+          url: "01 South Korea (ROK-US)/Pyeongtaek AAF (RKSG)/Pyeongtaek AAF (RKSG) - dvor_rwy_32.pdf",
+          page: 1
+        },
+        {
+          name: "RNAV RWY 32",
+          url: "01 South Korea (ROK-US)/Pyeongtaek AAF (RKSG)/Pyeongtaek AAF (RKSG) - rnav_gps_rwy_32.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 1057,
+    posy: 1671,
+    size: 8,
+    country: 'South Korea',
+    name: 'SACHEON',
+    type: 'Airbase',
+    details: {
+      name: "SACHEON (RKPS)",
+      lat: "N35°04.997'",
+      long: "E128°03.978'",
+      elev: "20 ft",
+      rwy: "06L/24R - 06R/24L",
+      tcn: "37X",
+      atis: "126.625",
+      ops: "",
+      gnd: "275.8",
+      twr: "305.4",
+      appdep: "317.425",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - ADC.pdf",
+          page: 1
+        },
+        {
+          name: "EOR Procedure Chart",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS)_EOR.png",
+          page: 1
+        },
+        {
+          name: "ILS RWY 06L (111.5)",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - ILS.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 24R (108.1)",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - ILS.pdf",
+          page: 17
+        },
+        {
+          name: "TACAN RWY 06L",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - ILS.pdf",
+          page: 5
+        },
+        {
+          name: "TACAN RWY 24L",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - ILS.pdf",
+          page: 29
+        },
+        {
+          name: "Standard Arrivals",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - STAR.pdf",
+          page: 1
+        },
+        {
+          name: "Standard Departures",
+          url: "01 South Korea (ROK-US)/Sacheon AB (RKPS)/Sacheon AB (RKPS) - SID.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 790,
+    posy: 1332,
+    size: 8,
+    country: 'South Korea',
+    name: 'SEOSAN',
+    type: 'Airbase',
+    details: {
+      name: "SEOSAN (RKTP)",
+      lat: "N36°42.343'",
+      long: "E126°29.265'",
+      elev: "42 ft",
+      rwy: "03L/21R - 03R/21L",
+      tcn: "73X",
+      atis: "130.3",
+      ops: "318.1",
+      gnd: "275.8",
+      twr: "353.1",
+      appdep: "253.95",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea/Seosan/Seosan_Airport.png",
+          page: 1
+        },
+        {
+          name: "EOR Procedure Chart",
+          url: "01 South Korea (ROK-US)/Seosan AB (RKTP)/Seosan AB (RKTP)_EOR.png",
+          page: 1
+        },
+        {
+          name: "ILS RWY 03R (111.5)",
+          url: "01 South Korea (ROK-US)/Seosan AB (RKTP)/Seosan AB (RKTP) - ilsdme_or_locdme_rwy_03r.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 21L (110.1)",
+          url: "01 South Korea (ROK-US)/Seosan AB (RKTP)/Seosan AB (RKTP) - ilsdme_or_locdme_rwy_21l.pdf",
+          page: 1
+        },
+        {
+          name: "HI-TACAN RWY 03L",
+          url: "01 South Korea (ROK-US)/Seosan AB (RKTP)/Seosan AB (RKTP) - hi_tacan_rwy_03l.pdf",
+          page: 1
+        },
+        {
+          name: "HI-TACAN RWY 03R",
+          url: "01 South Korea (ROK-US)/Seosan AB (RKTP)/Seosan AB (RKTP) - hi_tacan_rwy_03r.pdf",
+          page: 1
+        },
+        {
+          name: "HI-ILS RWY 03R",
+          url: "01 South Korea (ROK-US)/Seosan AB (RKTP)/Seosan AB (RKTP) - hi_ils_or_locdme_rwy_3r.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
+  {
+    posx: 896,
+    posy: 1180,
+    size: 8,
+    country: 'South Korea',
+    name: 'SEOUL',
+    type: 'Airbase',
+    details: {
+      name: "SEOUL AB (RKSM)",
+      lat: "N37°26.922'",
+      long: "E127°06.512'",
+      elev: "90 ft",
+      rwy: "01/19 - 20/02",
+      tcn: "49X",
+      atis: "126.4",
+      ops: "",
+      gnd: "276.2",
+      twr: "237.1",
+      appdep: "363.9",
+      charts: [
+        {
+          name: "Airport Diagram",
+          url: "01 South Korea (ROK-US)/Seoul AB (RKSM)/Seoul AB (RKSM) - ADC.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 20 (110.9)",
+          url: "01 South Korea (ROK-US)/Seoul AB (RKSM)/Seoul AB (RKSM) - ILS.pdf",
+          page: 1
+        },
+        {
+          name: "ILS RWY 19 (108.95)",
+          url: "01 South Korea (ROK-US)/Seoul AB (RKSM)/Seoul AB (RKSM) - ILS.pdf",
+          page: 7
+        },
+        {
+          name: "TACAN RWY 19",
+          url: "01 South Korea (ROK-US)/Seoul AB (RKSM)/Seoul AB (RKSM) - ILS.pdf",
+          page: 37
+        },
+        {
+          name: "TACAN RWY 20",
+          url: "01 South Korea (ROK-US)/Seoul AB (RKSM)/Seoul AB (RKSM) - ILS.pdf",
+          page: 41
+        },
+        {
+          name: "Standard Departures",
+          url: "01 South Korea (ROK-US)/Seoul AB (RKSM)/Seoul AB (RKSM) - SID.pdf",
+          page: 1
+        }
+      ]
+    }
+  },
   {posx: 878, posy: 1226, size: 6, country: 'South Korea', name: 'SUWON', type: 'Airbase'},
   {posx: 1038, posy: 1180, size: 6, country: 'South Korea', name: 'WONJU', type: 'Airbase'},
   {posx: 1152, posy: 1050, size: 6, country: 'South Korea', name: 'YANGYANG', type: 'Airbase'},
