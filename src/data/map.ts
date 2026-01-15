a8dffa83b456ff9db62c6206c322ff22e440bfff
Christian Gebauer <mail@christiangebauer.de>
2026-01-15 19:27:29 +0100

    fixed comments

--- src/data/map.ts
+++ src/data/map.ts
@@ -10,27 +10,27 @@ export const maps: Theater[] = [
     name: "korea",
     datum: {lat: 38.5, long: 127.18},
     feet: 3359580, // theater size, 1024 km
-    pixels: 6144, // map size
+    pixels: 6144, // map image size
     resolution: 3359580 / 6144, // approx 546.8
     px2nm: FtPerNM / (3359580 / 6144), // approx 11.11 pixels per NM
     folder: "04_KTO",
     mapFilename: "KTO_UI_Map_6k.jpeg", // native 6144
     stations: koreaStations,
     stationMappingSize: 4096,
-    local: true
+    local: true // load the map from own assets instead from bms cdn
   },
   {
     name: "balkans",
     datum: {lat: 36.625, long: 11}, // this might be v1 map datum
     feet: 3359580, // theater size, 1024 km
-    pixels: 6144, // map size
+    pixels: 6144, // map image size
     resolution: 3359580 / 6144, // approx 546.8
     px2nm: FtPerNM / (3359580 / 6144), // approx 11.11 pixels per NM
     folder: "02_Balkans",
-    mapFilename: "Balkans_UI_Map_6K.jpeg", // native 4096
+    mapFilename: "Balkans_UI_Map_6K.jpeg", // native 6144
     stations: balkanStations,
     stationMappingSize: 4096,
-    local: true
+    local: true // load the map from own assets instead from bms cdn
   }
 ]
 
