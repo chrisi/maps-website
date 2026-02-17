033547fa0371445d7cac2912bab790096291c034
Christian Gebauer <mail@christiangebauer.de>
2026-02-17 10:38:26 +0100

    erase button added, clear sketch functionality implemented WIP

--- src/components/whiteboard-window.vue
+++ src/components/whiteboard-window.vue
@@ -21,6 +21,12 @@ defineProps({
 
 const emit = defineEmits(['close'])
 
+const btnClearClick = () => {
+  global.whiteboard.shapes = []
+  global.whiteboard.symbols = []
+  // TODO: redraw whiteboard
+}
+
 const btnDownloadClick = () => {
   const blob = new Blob([JSON.stringify(global.whiteboard, null, 2)], {type: 'application/json'})
   const url = URL.createObjectURL(blob)
@@ -70,10 +76,12 @@ const tabChanged = (sender: string) => {
         <symbols-tab :visible="true"/>
       </template>
     </tool-tabs>
+    <tool-spacer/>
     <div style="display: flex; justify-content: space-between;">
       <tool-button id="downloadSketch" icon="/common/assets/icon_download.png" @click="btnDownloadClick" />
-      <tool-button id="inputMode_delete" icon="/common/icons/delete.png" @click="btnInputModeClick"
+      <tool-button id="inputMode_delete" icon="/common/icons/eraser.png" @click="btnInputModeClick"
                    :active="global.inputMode == InputMode.Delete"/>
+      <tool-button id="clearSketch" icon="/common/icons/delete.png" @click="btnClearClick" />
     </div>
   </tool-window>
 </template>
