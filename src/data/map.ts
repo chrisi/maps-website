516056d919515f0fa48c75b4b64e5dd74f074eab
Christian Gebauer <mail@christiangebauer.de>
2026-02-24 09:22:44 +0100

    korea stations update

--- src/data/map.ts
+++ src/data/map.ts
@@ -9,14 +9,16 @@ export const cdnUrl = "https://cdn.falcon-bms.com/maps"
 export const koreaStations = koreaStationsJson as Station[]
 export const balkansStations = balkansStationsJson as Station[]
 
+const MapSizeFeet = 3358699.5
+
 export const maps: Theater[] = [
   {
     name: "korea",
-    datum: {lat: 38.5, long: 127.18},
-    feet: 3359580, // theater size, 1024 km (3358699.5)
+    datum: {lat: 38.5, long: 127.5}, // 127.18
+    feet: MapSizeFeet, // theater size, 1024 km (3358699.5)
     pixels: 6144, // map image size
-    resolution: 3359580 / 6144, // approx 546.8
-    px2nm: FtPerNM / (3359580 / 6144), // approx 11.11 pixels per NM
+    resolution: MapSizeFeet / 6144, // approx 546.8
+    px2nm: FtPerNM / (MapSizeFeet / 6144), // approx 11.11 pixels per NM
     folder: "04_KTO",
     mapFilename: "KTO_UI_Map_6k.jpeg", // native 6144
     stations: koreaStations,
@@ -25,11 +27,11 @@ export const maps: Theater[] = [
   },
   {
     name: "balkans",
-    datum: {lat: 36.625, long: 11}, // this might be v1 map datum
-    feet: 3359580, // theater size, 1024 km
+    datum: {lat: 41.8327, long: 16.4191}, // this might be v1 map datum
+    feet: MapSizeFeet, // theater size, 1024 km
     pixels: 6144, // map image size
-    resolution: 3359580 / 6144, // approx 546.8
-    px2nm: FtPerNM / (3359580 / 6144), // approx 11.11 pixels per NM
+    resolution: MapSizeFeet / 6144, // approx 546.8
+    px2nm: FtPerNM / (MapSizeFeet / 6144), // approx 11.11 pixels per NM
     folder: "02_Balkans",
     mapFilename: "Balkans_UI_Map_6K.jpeg", // native 6144
     stations: balkansStations,
